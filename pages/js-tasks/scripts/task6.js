62fe3e4dd0d517ca2129ae7225f56f597d0e4227
floork <floork@gmx.de>
2022-12-02 11:10:27 +0100

    colors now resetting

--- pages/js-tasks/scripts/task6.js
+++ pages/js-tasks/scripts/task6.js
@@ -1,9 +1,31 @@
+var lastElem = 0;
+
 function adding() {
-  let shoeSize = document.querySelector("#inp").value;
-  let size = shoeSize.toString();
-  var elements = document.getElementsByClassName(size); // get all elements
-  for (var i = 0; i < elements.length; i++) {
-    elements[i].style.backgroundColor = "rgb(0, 128, 0)";
-    elements[i].innerHTML = '<img src="./../../../img/js-tasks/cupboard.svg" /></td>' + size;
+  if (lastElem == 0) {
+    let shoeSize = document.querySelector("#inp").value;
+    let size = shoeSize.toString();
+    var elements = document.getElementsByClassName(size); // get all elements
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.backgroundColor = "rgb(0, 128, 0)";
+      elements[i].innerHTML =
+        '<img src="./../../../img/js-tasks/cupboard.svg" /></td>' + size;
+    }
+    globalThis.lastElem = shoeSize;
+  } else {
+    let shoeSize = document.querySelector("#inp").value;
+    let size = shoeSize.toString();
+    var elements = document.getElementsByClassName(size); // get all elements
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].style.backgroundColor = "rgb(0, 128, 0)";
+      elements[i].innerHTML =
+        '<img src="./../../../img/js-tasks/cupboard.svg" /></td>' + size;
+    }
+    var elements2 = document.getElementsByClassName(lastElem); // get all elements
+    for (var i = 0; i < elements2.length; i++) {
+      elements2[i].style.backgroundColor = "rgb(165, 42, 42)";
+      elements2[i].innerHTML =
+        '<img src="./../../../img/js-tasks/cupboard.svg" /></td>';
+    }
+    globalThis.lastElem = shoeSize;
   }
 }
